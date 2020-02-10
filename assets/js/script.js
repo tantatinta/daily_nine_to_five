fb7a360e53cf22a82c36eeb3a3bd01802f7dd736
tantatinta <bluekasia@gmail.com>
2020-02-10 15:58:53 -0500

    updated JS with comments and local storage parse

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 var timeBlock = $(".timeBlock");
 var saveBtn = $(".saveBtn");
 var calendarItem = $(".calendarItem");
-var entries = [];
-var storedEntries;
+// var entries = [];
+// var storedEntries;
 
 
 var timeChange = setInterval(function() {
@@ -30,24 +30,37 @@ for (var i = 0; i < calendarItem.length; i++) {
 saveBtn.on("click", function(event){
     event.preventDefault();
     var textInput = $(this.dataset.number);
-    var tbId = $(this.dataset.number);
+    var tbId = this.dataset.number; //we want this to grab the id with the hashtag
 
     var plannerEntry = {
         text: textInput.val().trim(),
-        id: tbId.attr("id")
+        id: tbId
     }
+//we're checking that there is nothing inside the storage
+//we tell it to get our entry or an empty item. THEN we save our entries in array and local storage
+    var storedEntries = JSON.parse(localStorage.getItem("plannerEntry") || "[]")
     
-    entries.push(plannerEntry);
-    localStorage.setItem("plannerEntry", JSON.stringify(entries));
-
-    // for (var k = 0; k < entries.length; k++){
-    //     $().innerHTML.text = (entries[k].text);
-
-    //     console.log();
-    // }
-
-    // newEntry = JSON.parse(localStorage.getItem("entries"));
-    // var parsedText = newEntry.text;
-    // var parsedId = newEntry.id;
-    
+    storedEntries.push(plannerEntry);
+    localStorage.setItem("plannerEntry", JSON.stringify(storedEntries));       
 })
+//we retrieve our locl storage values and save them into an array
+ var parsedEntries = JSON.parse(localStorage.getItem("plannerEntry") || "[]");
+
+ //that looks like this
+//  parsedEntries = [
+//     {
+//         text: value,
+//         id: value
+//     },
+//     {
+//         text: value,
+//         id: value
+//     }
+//  ]
+// this is how we access our items
+//  parsedEntries[0].text
+//  parsedEntries[1].id
+// create for loop to select the textbox with the id value and replace its text with the text that corresponds to the same id number
+for (var m = 0; m < parsedEntries.length; m++) {
+$(parsedEntries[m].id).val(parsedEntries[m].text)
+}
